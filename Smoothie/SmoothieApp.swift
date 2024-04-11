85a8e85e7092c0db36c68bb41226adff370b7607
superlopez-real <kakskemail@gmail.com>
2024-04-11 17:00:08 +0100

    Initial Commit

---
+++ Smoothie/SmoothieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmoothieApp.swift
+//  Smoothie
+//
+//  Created by Kyan De Sutter on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SmoothieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
